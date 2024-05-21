75473bf83d2f4a41055dcd70cda9818faa3a3a67
Juan <jmoreno@novopayment.com>
2024-05-21 15:31:31 -0500

    Stub

---
+++ AsesoftwareTestSwift202405/AsesoftwareTestSwift202405/View/TMDBViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TMDBViewController.swift
+//  AsesoftwareTestSwift202405
+//
+//  Created by Juan Manuel Moreno on 21/05/24.
+//
+
+import UIKit
+
+class TMDBViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ AsesoftwareTestSwift202405/AsesoftwareTestSwift202405/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AsesoftwareTestSwift202405
+//
+//  Created by Juan Manuel Moreno on 21/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func begin(_ sender: Any) {
+        print("Aqui vamos a TMDBVC")
+    }
+    
+}
+
